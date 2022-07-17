801bf2d46ebb2ddeb3869913462b72d3cdf42c42
kleinay <ayal.s.klein@gmail.com>
2022-07-17 13:46:27 +0300

    small fix

--- qasem/__init__.py
+++ qasem/__init__.py
@@ -2,6 +2,6 @@ from pathlib import Path
 package_homedir = Path(__file__).parent
 data_dir = package_homedir / "data"
 # version
-with open("qasem/version.txt", "r") as f:
+with open(package_homedir / "version.txt", "r") as f:
     version = f.read().strip()
 __version__ = version
